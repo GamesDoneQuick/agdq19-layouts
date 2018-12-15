da1945ab76ad8d7ddefc9d4a191072011469623e
Alex Van Camp <vancamp.alex@gmail.com>
2018-12-15 16:49:49 -0600

    refactor(dash-producer): rename ui-obs-status to obs-system-status

---
+++ shared/elements/interfaces/ui-system-status/ui-system-status.js
@@ -0,0 +1,14 @@
+import * as tslib_1 from "tslib";
+const { customElement } = Polymer.decorators;
+/**
+ * @customElement
+ * @polymer
+ * @appliesMixin Polymer.MutableData
+ */
+let UiSystemStatusElement = class UiSystemStatusElement extends Polymer.MutableData(Polymer.Element) {
+};
+UiSystemStatusElement = tslib_1.__decorate([
+    customElement('ui-system-status')
+], UiSystemStatusElement);
+export default UiSystemStatusElement;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidWktc3lzdGVtLXN0YXR1cy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInVpLXN5c3RlbS1zdGF0dXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sRUFBQyxhQUFhLEVBQUMsR0FBRyxPQUFPLENBQUMsVUFBVSxDQUFDO0FBRTNDOzs7O0dBSUc7QUFFSCxJQUFxQixxQkFBcUIsR0FBMUMsTUFBcUIscUJBQXNCLFNBQVEsT0FBTyxDQUFDLFdBQVcsQ0FBQyxPQUFPLENBQUMsT0FBTyxDQUFDO0NBQUcsQ0FBQTtBQUFyRSxxQkFBcUI7SUFEekMsYUFBYSxDQUFDLGtCQUFrQixDQUFDO0dBQ2IscUJBQXFCLENBQWdEO2VBQXJFLHFCQUFxQiJ9
