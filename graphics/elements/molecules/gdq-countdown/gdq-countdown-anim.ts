3c834aa25337415086b8eb55bacc0bdc072030fc
Chris Hanel <chrishanel@gmail.com>
2018-12-22 19:04:35 -0800

    feat(countdown): new countdown animation

---
+++ graphics/elements/molecules/gdq-countdown/gdq-countdown-anim.ts
@@ -0,0 +1,26 @@
+const {customElement, property} = Polymer.decorators;
+
+/**
+ * @customElement
+ * @polymer
+ */
+@customElement('gdq-countdown-anim')
+export default class GDQCountdownAnim extends Polymer.Element {
+	@property({type: Number, notify: true})
+	active: number;
+
+	ready() {
+		super.ready();
+		this.activeLoop();
+	}
+
+	activeLoop() {
+		this.active = Math.floor(Math.random() * 8) + 1;
+		console.log("loop " + this.active);
+		setTimeout(this.activeLoop.bind(this), 3000);
+	}
+
+	// track and determine which square is 'active'
+	// every few seconds (within a random range), the active square changes
+	// this change should result in an adjacent square being picked
+}
