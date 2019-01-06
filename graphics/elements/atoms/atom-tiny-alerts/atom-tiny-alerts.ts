7da5cff75105fd25b907ca9bfae0947d368cf285
Alex Van Camp <vancamp.alex@gmail.com>
2019-01-06 12:28:43 -0500

    perf: re-enable tiny alerts

--- graphics/elements/atoms/atom-tiny-alerts/atom-tiny-alerts.ts
+++ graphics/elements/atoms/atom-tiny-alerts/atom-tiny-alerts.ts
@@ -9,7 +9,6 @@ export interface Alert {
 }
 
 const {customElement} = Polymer.decorators;
-const DISABLED = true;
 
 /**
  * @customElement
@@ -18,11 +17,6 @@ const DISABLED = true;
 @customElement('atom-tiny-alerts')
 export default class AtomTinyAlertsElement extends Polymer.Element {
 	addAlert({text, textColor = 'black', backgroundColor = 'white', holdDuration = 0.067}: Alert) {
-		if (DISABLED) {
-			const tl = new TimelineLite();
-			return tl;
-		}
-
 		const div = document.createElement('div');
 		div.classList.add('alert');
 		div.innerText = text;
